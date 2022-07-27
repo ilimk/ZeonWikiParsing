91edb88f31ee9bbb597a6cffe0b013f908b5f63b
ilimk <43859355+ilimk@users.noreply.github.com>
2022-07-27 21:24:40 +0600

    Update WikiCities.py

--- WikiCities.py
+++ WikiCities.py
@@ -1,7 +1,5 @@
-import bs4.element
 import requests
 import csv
-import pandas as pd
 from bs4 import BeautifulSoup
 
 urlWikipedia = 'https://ru.wikipedia.org/wiki/%D0%93%D0%BE%D1%80%D0%BE%D0%B4%D0%B0_%D0%9A%D0%B8%D1%80%D0%B3%D0%B8%D0%B7%D0%B8%D0%B8'
